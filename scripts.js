b6d55ebde79b9c840c7557ba44686c99843a622c
Dana L. Brown <dana.l.brown@gmail.com>
2019-03-22 15:49:22 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -82,7 +82,7 @@ $('input[type="checkbox"]').click(function() {
     if ($('input[type="checkbox"]:checked').length > 0) {
         $('.checkbox >div').hide();
         $('input[type="checkbox"]:checked').each(function() {
-            $('.checkbox >div[jobs=' + this.cbFT + ']').show();
+            $('.checkbox >div[type=' + this.cbFT + ']').show();
         });
     } else {
         $('.checkbox >div').show();

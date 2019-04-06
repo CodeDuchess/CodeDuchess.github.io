a1e3cd7341f6fd84b767d4e6d26b43ec113e6e9a
Dana L. Brown <dana.l.brown@gmail.com>
2019-04-06 16:21:24 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 // USED STRICT MODE TO SOLVE: “UNCAUGHT SYNTAX ERROR: UNEXPECTED TOKEN U IN JSON @ POSITION 0”
 'use strict';
 
@@ -9,11 +10,11 @@ app.appendChild(container1);
 var request = new XMLHttpRequest();
 
 //GET REQUEST WITH USE OF HEROKU AS A PROXY TO SOLVE CORS ERROR
-request.open('GET','https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?&markdown=true&location=united+states&page=1&per_page=20', 
+request.open('GET','https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?&markdown=true&location=united+states&page=1&count=20', 
 true);
 
 request.onload = function () {  
-// CONVERT JSON DATA TO JAVASCRIPT OBJECTS USING JSON.PARSE
+//CONVERT JSON DATA TO JAVASCRIPT OBJECTS USING JSON.PARSE
     var apiData = JSON.parse(this.response);
     if (request.status >= 200 && request.status < 400) {
         apiData.forEach(job => {
@@ -22,8 +23,7 @@ request.onload = function () {
 
       // render relevant data: job title, job type, location and description      
         const h1 = document.createElement('h1');
-        h1.textContent = job.title.substring(0, 50);
-     
+        h1.textContent = job.title;
             
         const h3 = document.createElement('h3');
         h3.textContent = job.type;
@@ -53,17 +53,18 @@ request.onload = function () {
 
 
 // FILTER BY JOB TITLE
-var button1 = document.getElementById("keywordSearchButton");
-var userInput1 = document.getElementById("userInput1");
-button1.addEventListener('click', showResults1);
-userInput1.addEventListener("keyup", function(e){
+
+var button = document.querySelector("button");
+var userInput = document.getElementById("userInput");
+button.addEventListener('click', showResults);
+userInput.addEventListener("keyup", function(e){
   if (e.keyCode === 13){
     showResults();
   }
 })
 
-function showResults1(){
-  var searchKeyword = userInput1.value.toLowerCase();
+function showResults(){
+  var searchKeyword = userInput.value.toLowerCase();
   var cards = document.getElementsByClassName("card"); //this is an array
   var regex = new RegExp(searchKeyword, "g");
 
@@ -76,145 +77,27 @@ function showResults1(){
   }
 }
 
-// FILTER BY JOB LOCATION
-var button2 = document.getElementById("locationSearchButton");
-var userInput2 = document.getElementById("userInput2");
-button2.addEventListener('click', showResults2);
-userInput2.addEventListener("keyup", function(e){
-  if (e.keyCode === 13){
-    showResults();
-  }
-})
+// FILTER BY JOB TYPE USING JQUERY OPTION 1
+$('input[type="checkbox"]').click(function() {
+    if ($('input[type="checkbox"]:checked').length > 0) {
+        $('.checkbox >div').hide();
+        $('input[type="checkbox"]:checked').each(function() {
+            $('.checkbox >div[type=' + this.cbFT + ']').show();
+        });
+    } else {
+        $('.checkbox >div').show();
 
-function showResults2(){
-  var searchKeyword = userInput2.value.toLowerCase();
-  var cards = document.getElementsByClassName("card");
-  var jobLocations = document.getElementsByTagName("h4");
-  var regex = new RegExp(searchKeyword, "g");
-
-  for (var i=0; i<cards.length ; i++){
-    if (jobLocations[i].textContent.toLowerCase().match(regex)){
-      cards[i].style.display = "block";
-    } else{
-      cards[i].style.display= "none";
     }
-  }
-}
-
-// FILTER BY JOB TYPE 
-var fullTimeCheckBox = document.getElementById("cbFT");
-var partTimeCheckBox = document.getElementById("cbPT");
-var contractCheckBox = document.getElementById("cbCT");
-var jobType = document.getElementsByTagName("h3");
-
-fullTimeCheckBox.addEventListener("change", showByJobType);
-partTimeCheckBox.addEventListener("change", showByJobType);
-contractCheckBox.addEventListener("change", showByJobType);
-
-function showByJobType(){
-  var cards = document.getElementsByClassName("card");
-  for (var i=0; i<cards.length ; i++){
-    if (fullTimeCheckBox.checked === false){
-      if (jobType[i].textContent.toLowerCase() == "full time"){
-        cards[i].style.display = "none";
-      }
-    } else if (fullTimeCheckBox.checked){
-      if (jobType[i].textContent.toLowerCase() == "full time"){
-        cards[i].style.display = "block";
-      }
-    }
-    if (partTimeCheckBox.checked === false){
-      if (jobType[i].textContent.toLowerCase() == "part time"){
-        cards[i].style.display = "none";
-      }
-    }else if (partTimeCheckBox.checked){
-      if (jobType[i].textContent.toLowerCase() == "part time"){
-        cards[i].style.display = "block";
-      }
-    }
-    if (contractCheckBox.checked === false){
-      if (jobType[i].textContent.toLowerCase() == "contract"){
-        cards[i].style.display = "none";
-      }
-    }else if (contractCheckBox.checked){
-      if (jobType[i].textContent.toLowerCase() == "contract"){
-        cards[i].style.display = "block";
-      }
-    }
-  }
-}
-
-// PAGINATION
-
-    var list = new Array();
-    var pageList = new Array();
-    var currentPage = 1;
-    var numberPerPage = 20;
-    var numberOfPages = 0;
-    var x = "";
-    var r = "";
-
-function makeList() {
-    for (x = 0; x < 200; x++)
-        list.push(x);
-
-    numberOfPages = getNumberOfPages();
-}
-    
-function getNumberOfPages() {
-    return Math.ceil(list.length / numberPerPage);
-}
-
-function nextPage() {
-    currentPage += 1;
-    loadList();
-}
-
-function previousPage() {
-    currentPage -= 1;
-    loadList();
-}
-
-function firstPage() {
-    currentPage = 1;
-    loadList();
-}
-
-function lastPage() {
-    currentPage = numberOfPages;
-    loadList();
-}
-
-function loadList() {
-    var begin = ((currentPage - 1) * numberPerPage);
-    var end = begin + numberPerPage;
-
-    pageList = list.slice(begin, end);
-    drawList();
-    check();
-}
-    
-function drawList() {
-    document.getElementById("list").innerHTML = "";
-    for (r = 0; r < pageList.length; r++) {
-        document.getElementById("list").innerHTML += pageList[r] + "<br/>";
-    }
-}
-
-function check() {
-    document.getElementById("next").disabled = currentPage == numberOfPages ? true : false;
-    document.getElementById("previous").disabled = currentPage == 1 ? true : false;
-    document.getElementById("first").disabled = currentPage == 1 ? true : false;
-    document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
-}
-
-function load() {
-    makeList();
-    loadList();
-}
-    
-window.onload = load;
-
+});
 
 
 request.send();
+
+
+
+
+
+
+
+
+    
